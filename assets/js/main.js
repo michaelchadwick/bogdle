89c12be8573a383c8cfaf748fe4a035e2ab81740
Michael Chadwick <michael.chadwick@gmail.com>
2022-04-11 13:07:26 -0700

    lots of refactoring; some color changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -139,7 +139,7 @@ function modalOpen(type) {
       break
     case 'show-list':
       this.myModal = new Modal('perm-debug', 'Master Word List',
-        getSolutionSetDisplay(),
+        getGameSolution(),
         null,
         null
       )
@@ -204,7 +204,7 @@ function getGameProgress() {
   return html
 }
 
-function getSolutionSetDisplay() {
+function getGameSolution() {
   var html = '<ul>'
 
   // check each length category (MAX_WORD_LENGTH...3, etc.)
@@ -231,29 +231,6 @@ function getSolutionSetDisplay() {
   return html
 }
 
-// remove last letter in DOM guess div
-function removeLastLetter() {
-  if (this.bogdle.config.gameState == 'IN_PROGRESS') {
-    // remove last position from selected array
-    if (this.bogdle.tilesSelected.length) {
-      var last = this.bogdle.tilesSelected.pop()
-
-      Array.from(this.bogdle.tiles).forEach(tile => {
-        if (tile.dataset.pos == last) {
-          tile.dataset.state = 'tbd'
-        }
-      })
-    }
-
-    // remove last letter of active guess
-    if (this.bogdle.guess.innerHTML.length) {
-      this.bogdle.guess.innerHTML = this.bogdle.guess.innerHTML.slice(0, this.bogdle.guess.innerHTML.length - 1)
-
-      _checkGuess()
-    }
-  }
-}
-
 // submit a guess
 function submitWord(word) {
   // console.log('submitting word...', word)
@@ -268,10 +245,9 @@ function submitWord(word) {
           this.bogdle.config.lastPlayedTime = new Date().getTime()
           this.bogdle.statistics.wordsFound += 1
 
-          saveState()
+          _saveState()
 
           _increaseScore()
-          _resetInput()
 
           _checkWinState()
         } else {
@@ -301,20 +277,6 @@ function getGuessedWords() {
   return solutionKeys.filter(k => this.bogdle.solutionSet[k])
 }
 
-// save state from code model -> LS
-function saveState() {
-  // console.log('saving to localStorage...')
-
-  // save game state
-  try {
-    localStorage.setItem(LS_STATE_KEY, JSON.stringify(this.bogdle.config))
-
-    // console.log('!localStorage progress saved!', JSON.parse(localStorage.getItem(LS_STATE_KEY)))
-  } catch(error) {
-    console.error(`localStorage could not be set for ${LS_STATE_KEY}`, error)
-  }
-}
-
 // save statistics from code model -> LS
 function saveStats() {
   try {
@@ -324,65 +286,6 @@ function saveStats() {
   } catch(error) {
     console.error(`localStorage could not be set for ${LS_STATS_KEY}`, error)
   }
-}
-
-// load state/statistics from LS -> code model
-function loadState() {
-  // console.log('loading progress...')
-
-  // load game state
-  if (localStorage.getItem(LS_STATE_KEY)) {
-    // console.log('localStorage key found and loading...')
-
-    var lsConfig = JSON.parse(localStorage.getItem(LS_STATE_KEY))
-
-    // set game state
-    this.bogdle.config.gameState = lsConfig.gameState
-
-    // check off any pre-guessed words
-    this.bogdle.guessedWords = []
-    lsConfig.guessedWords.forEach(word => {
-      this.bogdle.config.guessedWords.push(word)
-      this.bogdle.solutionSet[word.length][word] = 1
-    })
-    this.bogdle.guessedWords.sort()
-
-    // set last completed
-    this.bogdle.config.lastCompletedTime = lsConfig.lastCompletedTime
-    // set last played
-    this.bogdle.config.lastPlayedTime = lsConfig.lastPlayedTime
-
-    // never played before? show help modal
-    if (this.bogdle.config.lastPlayedTime == null) {
-      modalOpen('help')
-    }
-
-    // console.log('!localStoragekey loaded!', JSON.parse(localStorage.getItem(LS_STATE_KEY)))
-  } else {
-    // console.log('no localStorage key found; defaults being set')
-    modalOpen('help')
-
-    saveState()
-  }
-
-  // load user statistics
-  if (localStorage.getItem(LS_STATS_KEY)) {
-    var lsConfig = JSON.parse(localStorage.getItem(LS_STATS_KEY))
-
-    this.bogdle.statistics = {
-      "gamesPlayed": lsConfig.gamesPlayed,
-      "wordsFound": lsConfig.wordsFound
-    }
-  }
-
-  // load game settings
-  _loadSettings()
-
-  _setScore(this.bogdle.config.guessedWords.length)
-
-  _checkWinState()
-
-  // console.log('!progress loaded!', this.bogdle.solutionSet)
 }
 
 // start the engine
@@ -482,7 +385,7 @@ async function _loadTestSolutionSet(newWord) {
       // choose letters randomly from solution set
       _shuffleTiles()
 
-      loadState()
+      _loadState()
     }
   } catch (err) {
     console.error('could not create new Findle', err)
@@ -556,7 +459,7 @@ async function _loadRealSolutionSet() {
 
         _resetProgress()
 
-        // loadState()
+        // _loadState()
       }
     } catch (err) {
       console.error('could not create new Findle', err)
@@ -617,7 +520,7 @@ function _resetProgress() {
   }
 
   // save those defaults to local storage
-  saveState()
+  _saveState()
 
   // set solution set statuses back to 0
   Object.keys(this.bogdle.solutionSet).forEach(key => {
@@ -711,10 +614,10 @@ function _checkGuess() {
         // and it's the first time
         if (!this.bogdle.solutionSet[key][word]) {
           this.bogdle.guess.classList.add('first-guess')
+          animateCSS('#guess', 'pulse')
         }
       } else {
         // player guessed an invalid word (not on list)
-
       }
     })
   } else {
@@ -747,7 +650,7 @@ function _checkWinState() {
       this.bogdle.config.lastCompletedTime = new Date().getTime()
     }
 
-    saveState()
+    _saveState()
 
     // disable inputs (until future daily re-enabling)
     _disableTiles()
@@ -758,15 +661,22 @@ function _checkWinState() {
 
 // reset UI tiles to default state
 function _resetInput() {
-  // reset tiles
+  _resetTiles()
+
+  _resetGuess()
+}
+// disable all UI tiles
+function _disableTiles() {
+  Array.from(this.bogdle.tiles).forEach(tile => {
+    tile.setAttribute('disabled', '')
+    tile.dataset.state = 'disabled'
+  })
+}
+function _resetTiles() {
   Array.from(this.bogdle.tiles).forEach(tile => {
     tile.dataset.state = 'tbd'
   })
-  // reset guess
-  this.bogdle.guess.innerHTML = ''
-  this.bogdle.guess.classList.remove('valid')
 }
-// shuffle all UI tiles
 function _shuffleTiles() {
   let letters = this.bogdle.letters
 
@@ -787,13 +697,32 @@ function _shuffleTiles() {
   // make sure game is playable again
   _resetInput()
 }
+function _resetGuess() {
+  this.bogdle.guess.innerHTML = ''
+  this.bogdle.guess.classList.remove('valid')
+}
 
-// disable all UI tiles
-function _disableTiles() {
-  Array.from(this.bogdle.tiles).forEach(tile => {
-    tile.setAttribute('disabled', '')
-    tile.dataset.state = 'disabled'
-  })
+// remove last letter in DOM guess div
+function _removeLastLetter() {
+  if (this.bogdle.config.gameState == 'IN_PROGRESS') {
+    // remove last position from selected array
+    if (this.bogdle.tilesSelected.length) {
+      var last = this.bogdle.tilesSelected.pop()
+
+      Array.from(this.bogdle.tiles).forEach(tile => {
+        if (tile.dataset.pos == last) {
+          tile.dataset.state = 'tbd'
+        }
+      })
+    }
+
+    // remove last letter of active guess
+    if (this.bogdle.guess.innerHTML.length) {
+      this.bogdle.guess.innerHTML = this.bogdle.guess.innerHTML.slice(0, this.bogdle.guess.innerHTML.length - 1)
+
+      _checkGuess()
+    }
+  }
 }
 
 // dynamically resize board depending on viewport
@@ -849,6 +778,77 @@ function _toggleSetting(setting) {
 function _saveSetting(setting, value) {
   localStorage.setItem(setting, value)
 }
+// save state from code model -> LS
+function _saveState() {
+  // console.log('saving to localStorage...')
+
+  // save game state
+  try {
+    localStorage.setItem(LS_STATE_KEY, JSON.stringify(this.bogdle.config))
+
+    // console.log('!localStorage progress saved!', JSON.parse(localStorage.getItem(LS_STATE_KEY)))
+  } catch(error) {
+    console.error(`localStorage could not be set for ${LS_STATE_KEY}`, error)
+  }
+}
+// load state/statistics from LS -> code model
+function _loadState() {
+  // console.log('loading progress...')
+
+  // load game state
+  if (localStorage.getItem(LS_STATE_KEY)) {
+    // console.log('localStorage key found and loading...')
+
+    var lsConfig = JSON.parse(localStorage.getItem(LS_STATE_KEY))
+
+    // set game state
+    this.bogdle.config.gameState = lsConfig.gameState
+
+    // check off any pre-guessed words
+    this.bogdle.guessedWords = []
+    lsConfig.guessedWords.forEach(word => {
+      this.bogdle.config.guessedWords.push(word)
+      this.bogdle.solutionSet[word.length][word] = 1
+    })
+    this.bogdle.guessedWords.sort()
+
+    // set last completed
+    this.bogdle.config.lastCompletedTime = lsConfig.lastCompletedTime
+    // set last played
+    this.bogdle.config.lastPlayedTime = lsConfig.lastPlayedTime
+
+    // never played before? show help modal
+    if (this.bogdle.config.lastPlayedTime == null) {
+      modalOpen('help')
+    }
+
+    // console.log('!localStoragekey loaded!', JSON.parse(localStorage.getItem(LS_STATE_KEY)))
+  } else {
+    // console.log('no localStorage key found; defaults being set')
+    modalOpen('help')
+
+    _saveState()
+  }
+
+  // load user statistics
+  if (localStorage.getItem(LS_STATS_KEY)) {
+    var lsConfig = JSON.parse(localStorage.getItem(LS_STATS_KEY))
+
+    this.bogdle.statistics = {
+      "gamesPlayed": lsConfig.gamesPlayed,
+      "wordsFound": lsConfig.wordsFound
+    }
+  }
+
+  // load game settings
+  _loadSettings()
+
+  _setScore(this.bogdle.config.guessedWords.length)
+
+  _checkWinState()
+
+  // console.log('!progress loaded!', this.bogdle.solutionSet)
+}
 
 // add event listeners to DOM
 function _addEventListeners() {
@@ -871,7 +871,7 @@ function _addEventListeners() {
 
   // ⌫ backspace
   this.bogdle.buttons.btnBackspace.addEventListener('click', () => {
-    removeLastLetter()
+    _removeLastLetter()
   })
 
   // X clear
@@ -920,7 +920,7 @@ function _addEventListeners() {
       submitWord(this.bogdle.guess.innerHTML)
     }
     if (event.code == 'Backspace' || event.code == 'Delete') {
-      removeLastLetter()
+      _removeLastLetter()
     }
 
     var validLetters = this.bogdle.letters.map(l => l.toUpperCase())
