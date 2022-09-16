b7e7e0f5c689d005997a12aa725f38848aca528f
Michael Chadwick <michael.chadwick@gmail.com>
2022-09-16 09:32:56 -0700

    linting

--- assets/js/app/dom.js
+++ assets/js/app/dom.js
@@ -1,48 +1,48 @@
 /* dom */
 /* grab references to dom elements */
-/* global $, Bogdle */
+/* global Bogdle */
 
 // DOM > main divs/elements
 Bogdle.dom = {
-  "navOverlay": document.getElementById('nav-overlay'),
-  "navContent": document.getElementById('nav-content'),
-  "dailyDetails": document.getElementById('daily-details'),
-  "guess": document.getElementById('guess'),
-  "score": document.getElementById('score-container'),
-  "scoreGuessed": document.getElementById('score-guessed'),
-  "scoreGuessedOf": document.getElementById('score-guessed-of'),
-  "scoreTotal": document.getElementById('score-total'),
-  "scoreTotalWords": document.getElementById('score-total-words'),
+  'navOverlay': document.getElementById('nav-overlay'),
+  'navContent': document.getElementById('nav-content'),
+  'dailyDetails': document.getElementById('daily-details'),
+  'guess': document.getElementById('guess'),
+  'score': document.getElementById('score-container'),
+  'scoreGuessed': document.getElementById('score-guessed'),
+  'scoreGuessedOf': document.getElementById('score-guessed-of'),
+  'scoreTotal': document.getElementById('score-total'),
+  'scoreTotalWords': document.getElementById('score-total-words'),
 }
 
 // DOM > interactive elements
 Bogdle.dom.interactive = {
-  "btnNav": document.getElementById('button-nav'),
-  "btnNavClose": document.getElementById('button-nav-close'),
-  "btnHelp": document.getElementById('button-help'),
-  "btnStats": document.getElementById('button-stats'),
-  "btnSettings": document.getElementById('button-settings'),
-  "difficultyContainer": document.getElementById('difficulty-container'),
-  "difficultyContainerLinks": document.querySelectorAll('#difficulty-container a'),
-  "gameModeDailyLink": document.getElementById('gamemode-0'),
-  "gameModeFreeLink": document.getElementById('gamemode-1'),
-  "btnSubmit": document.getElementById('button-submit'),
-  "btnBackspace": document.getElementById('button-backspace'),
-  "btnClearGuess": document.getElementById('button-clear-guess'),
-  "btnShuffle": document.getElementById('button-shuffle'),
-  "btnShowProgress": document.getElementById('button-show-progress'),
-  "btnGuessLookup": document.getElementById('button-guess-lookup'),
-  "btnCreateNew": document.getElementById('button-create-new'),
-  "btnHint": document.getElementById('button-hint'),
-  "btnHintReset": document.getElementById('button-hint-reset'),
-  "tiles": document.getElementsByClassName('tile')
+  'btnNav': document.getElementById('button-nav'),
+  'btnNavClose': document.getElementById('button-nav-close'),
+  'btnHelp': document.getElementById('button-help'),
+  'btnStats': document.getElementById('button-stats'),
+  'btnSettings': document.getElementById('button-settings'),
+  'difficultyContainer': document.getElementById('difficulty-container'),
+  'difficultyContainerLinks': document.querySelectorAll('#difficulty-container a'),
+  'gameModeDailyLink': document.getElementById('gamemode-0'),
+  'gameModeFreeLink': document.getElementById('gamemode-1'),
+  'btnSubmit': document.getElementById('button-submit'),
+  'btnBackspace': document.getElementById('button-backspace'),
+  'btnClearGuess': document.getElementById('button-clear-guess'),
+  'btnShuffle': document.getElementById('button-shuffle'),
+  'btnShowProgress': document.getElementById('button-show-progress'),
+  'btnGuessLookup': document.getElementById('button-guess-lookup'),
+  'btnCreateNew': document.getElementById('button-create-new'),
+  'btnHint': document.getElementById('button-hint'),
+  'btnHintReset': document.getElementById('button-hint-reset'),
+  'tiles': document.getElementsByClassName('tile')
 }
 // DOM > interactive elements (debug)
 Bogdle.dom.interactive.debug = {
-  "all": document.getElementById('debug-buttons'),
-  "btnShowList": document.getElementById('button-show-solution'),
-  "btnResetProgress": document.getElementById('button-reset-progress'),
-  "btnShowConfig": document.getElementById('button-show-config'),
-  "btnShowState": document.getElementById('button-show-state'),
-  "btnWinGame": document.getElementById('button-win-game')
+  'all': document.getElementById('debug-buttons'),
+  'btnShowList': document.getElementById('button-show-solution'),
+  'btnResetProgress': document.getElementById('button-reset-progress'),
+  'btnShowConfig': document.getElementById('button-show-config'),
+  'btnShowState': document.getElementById('button-show-state'),
+  'btnWinGame': document.getElementById('button-win-game')
 }
